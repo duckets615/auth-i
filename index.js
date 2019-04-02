a0562692130beb71267af808f074a81753b3384e
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-04-02 18:24:56 -0400

    login post

--- index.js
+++ index.js
@@ -43,4 +43,29 @@ server.post('/register', (req, res) => {
     });
 });
 
+server.post('/login', (req, res) => {
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        req.session.username = user.username;
+        res
+          .status(200)
+          .json({ welcome: user.username });
+      } else {
+        res
+          .status(401)
+          .json({ message: 'XX nope, your not getting in with that password XX'})
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ err });
+    })
+})
+
 server.listen(port, () => console.log(`\n==  Project rolling on port ${port} ==\n`))
